dfcb51d3d4daef3e9c09e2909cc57695e5958e07
Ruhaim Mohammed <ruhaimmohammed@gmail.com>
2021-10-12 23:04:24 +0530

    Style added

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// update on range change 
 let amount = document.getElementById("amount");
 let year = document.getElementById("year");
 let investedAmount = document.getElementById("invested-amount");
@@ -5,72 +6,43 @@ let profit = document.getElementById("returns");
 let range = document.getElementById("range");
 
 year.value = 5 ;
-amount.value = 1000 ;
+amount.value = 25000 ;
 range.value = 10;
 document.getElementById("range-value").innerHTML = range.value ;
 
-
-// update on page load 
 addEventListener('load', () => {
-    rangeDisplay = range.value ;
-    document.getElementById("range-value").innerHTML = rangeDisplay ;
-
-
     let investAmount = amount.value * 12 * year.value ;
-    investedAmount.innerHTML = "Invested Amount :" + investAmount;
+    investedAmount.innerHTML = "Invested Amount: " + investAmount;
 
     let i = (range.value / 12) / 100 ;
     let n = year.value * 12 ;
     let x = Math.pow(1 + i , n)
 
     let sum = amount.value * ((x - 1) / i ) * (1 + i) ;
-    profit.innerHTML = Math.floor(sum) ;
+    profit.innerHTML ="ROI: " + Math.floor(sum) ;
   });
 
-// update on range change 
 range.addEventListener('input', () => {
     rangeDisplay = range.value ;
     document.getElementById("range-value").innerHTML = rangeDisplay ;
 
-
-    let investAmount = amount.value * 12 * year.value ;
-    investedAmount.innerHTML = "Invested Amount :" + investAmount;
-
-    let i = (range.value / 12) / 100 ;
-    let n = year.value * 12 ;
-    let x = Math.pow(1 + i , n)
-
-    let sum = amount.value * ((x - 1) / i ) * (1 + i) ;
-    profit.innerHTML = Math.floor(sum) ;
   });
 
 
-// update on year change 
-year.addEventListener('input', () => { 
+
+function calculator() { 
     let investAmount = amount.value * 12 * year.value ;
-    investedAmount.innerHTML = "Invested Amount :" + investAmount;
+    investedAmount.innerHTML = "Invested Amount: " + investAmount;
 
     let i = (range.value / 12) / 100 ;
     let n = year.value * 12 ;
     let x = Math.pow(1 + i , n)
 
     let sum = amount.value * ((x - 1) / i ) * (1 + i) ;
-    profit.innerHTML = Math.floor(sum) ;
+    profit.innerHTML = "ROI: " + Math.floor(sum) ;
+    applause.innerHTML = "HEY CONGRATULATIONS, That's a lot of money and you are rich now. 💸💸👏👏 "
  
 
-  });
+  }
 
-// update on amount change 
-amount.addEventListener('input', () => { 
-    let investAmount = amount.value * 12 * year.value ;
-    investedAmount.innerHTML = "Invested Amount :" + investAmount;
 
-    let i = (range.value / 12) / 100 ;
-    let n = year.value * 12 ;
-    let x = Math.pow(1 + i , n)
-
-    let sum = amount.value * ((x - 1) / i ) * (1 + i) ;
-    profit.innerHTML = Math.floor(sum) ;
- 
-
-  });
