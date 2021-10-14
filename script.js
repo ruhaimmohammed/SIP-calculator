19b8bbba23be48d1fe8239828876823ef9dec5f9
Ruhaim Mohammed <ruhaimmohammed@gmail.com>
2021-10-14 23:57:51 +0530

    Standardaized code

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// update on range change 
 let amount = document.getElementById("amount");
 let year = document.getElementById("year");
 let investedAmount = document.getElementById("invested-amount");
@@ -10,17 +9,7 @@ amount.value = 25000 ;
 range.value = 10;
 document.getElementById("range-value").innerHTML = range.value ;
 
-addEventListener('load', () => {
-    let investAmount = amount.value * 12 * year.value ;
-    investedAmount.innerHTML = "Invested Amount: " + investAmount;
-
-    let i = (range.value / 12) / 100 ;
-    let n = year.value * 12 ;
-    let x = Math.pow(1 + i , n)
-
-    let sum = amount.value * ((x - 1) / i ) * (1 + i) ;
-    profit.innerHTML ="ROI: " + Math.floor(sum) ;
-  });
+// Event listener for displaying value of range 
 
 range.addEventListener('input', () => {
     rangeDisplay = range.value ;
@@ -28,21 +17,30 @@ range.addEventListener('input', () => {
 
   });
 
+// function to calculate SIP value 
 
+function calculate(){
+  let investAmount = amount.value * 12 * year.value ;
+  investedAmount.innerHTML = "Invested Amount: " + investAmount;
+
+  let i = (range.value / 12) / 100 ;
+  let n = year.value * 12 ;
+  let x = Math.pow(1 + i , n)
+
+  let sum = amount.value * ((x - 1) / i ) * (1 + i) ;
+  profit.innerHTML ="ROI: " + Math.floor(sum) ;
+}
+
+// Event Listener on page load to calculate default value 
+
+addEventListener('load', () => {
+  calculate();
+  });
+
+// funtion to trigger on button click
 
 function calculator() { 
-    let investAmount = amount.value * 12 * year.value ;
-    investedAmount.innerHTML = "Invested Amount: " + investAmount;
-
-    let i = (range.value / 12) / 100 ;
-    let n = year.value * 12 ;
-    let x = Math.pow(1 + i , n)
-
-    let sum = amount.value * ((x - 1) / i ) * (1 + i) ;
-    profit.innerHTML = "ROI: " + Math.floor(sum) ;
+    calculate();
     applause.innerHTML = "HEY CONGRATULATIONS, That's a lot of money and you are rich now. 💸💸👏👏 "
  
-
-  }
-
-
+}
